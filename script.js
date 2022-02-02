d92d884ba358ef161be8ab5987af1c4d04960726
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-02-02 13:41:24 -0500

    22th commit // jquery

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 
+//jquery for carousel pause and play buttons:
+
+$(function(){
+    $(".carousel").carousel({interval: 2000});
+    $("#carouselPause").click(function(){
+        $(".carousel").carousel("pause");
+    });
+    $("#carouselPlay").click(function(){
+        $(".carousel").carousel("cycle");
+    });
+});
+
+//
+
 
